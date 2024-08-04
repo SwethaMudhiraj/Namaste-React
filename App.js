5c9b751ad74933333310849e9ef0cd1961cc67e5
swetha <swethadontharaboina0007@gmail.com>
2024-08-04 15:30:58 +0530

    chapter-01

---
+++ App.js
@@ -0,0 +1,28 @@
+  
+  
+  
+  const heading =  React.createElement("div",{id:"parent"},
+       [React.createElement("div",{id:"child1"},
+        [React.createElement("h1",{},"Iam h1 tag"),
+            React.createElement("h2",{},"Iam h2 tag")
+        ] ,
+
+    ),
+       React.createElement("div",{id:"child2"},
+        [React.createElement("h1",{},"Iam h1 tag"),
+            React.createElement("h2",{},"Iam h2 tag")
+        ] 
+   ) ]
+
+  )
+     
+     
+      
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading)
